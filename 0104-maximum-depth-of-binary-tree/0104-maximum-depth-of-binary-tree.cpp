3f2fe67fa64ba157f8c99baa375e4d0bbad72547
Aryan Singh <aryansingh16337@gmail.com>
2026-08-03 16:22:50 +0530

    Time: 0 ms (100%), Space: 22.2 MB (15.38%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -16,6 +16,7 @@ public:
 
         int lh = maxDepth(root->left);
         int rh  = maxDepth(root->right);
+        int diameter = max(diameter , lh + rh);
 
         return max(lh , rh) + 1;
         
